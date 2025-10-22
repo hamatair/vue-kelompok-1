86f02e9c9b9f8a68565144ceb7a546145e8a576b
Muhamad Raffi <raffii450@gmail.com>
2025-10-22 08:53:25 +0700

    tailwind

--- vue-kelompok-1/src/App.vue
+++ vue-kelompok-1/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import './assets/main.css'
 </script>
 
 <template>
---
+++ vue-kelompok-1/tailwind.config.ts
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/views.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      // custom styles di sini
+    },
+  },
+  plugins: [],
+}
