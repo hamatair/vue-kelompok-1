990c03cf79b425c03dd1f0ff18b58331326d3f91
ilham <akabeaer212@gmail.com>
2025-11-24 20:42:04 +0700

    refactor : add axios, pinia, and vue-router@4

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const pinia = createPinia()
 
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(pinia).use(router).mount('#app')
