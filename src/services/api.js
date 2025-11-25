68d97b95816d6207ccfe32180aa0646f6e2eab7a
ilham <akabeaer212@gmail.com>
2025-11-25 22:03:19 +0700

    refactor : service/api

--- src/services/api.js
+++ src/services/api.js
@@ -1,22 +1,21 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api/medical-records";
+const API_URL = "http://localhost:8000/api";
 
 // RIWAYAT KESEHATAN
-export const getMedicalRecords = () => axios.get(API_URL);
-export const getMedicalRecord = (id) => axios.get(`${API_URL}/${id}`);
+export const getMedicalRecords = () => axios.get(`${API_URL}/medical-records`);
+
+// export const getMedicalRecord = (id) =>
+//   axios.get(`${API_URL}/medical-records/${id}`);
+
 export const createMedicalRecord = (data) =>
-  axios.post(API_URL, data, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
+  axios.post(`${API_URL}/medical-records`, data);
+
 export const updateMedicalRecord = (id, data) =>
-  axios.post(`${API_URL}/${id}?_method=PUT`, data, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
-export const patchMedicalRecord = (id, data) =>
-  axios.patch(`${API_URL}/${id}`, data);
-export const deleteMedicalRecord = (id) => axios.delete(`${API_URL}/${id}`);
+  axios.put(`${API_URL}/medical-records/${id}`, data);
+
+// export const patchMedicalRecord = (id, data) =>
+//   axios.patch(`${API_URL}/medical-records/${id}`, data);
+
+export const deleteMedicalRecord = (id) =>
+  axios.delete(`${API_URL}/medical-records/${id}`);
